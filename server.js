fcbf6fc8af1dde89ff7063a360b3839a36aa3759
drg0drg <60710786+drg0drg@users.noreply.github.com>
2020-06-12 20:47:52 +0100

    npm i path. Routes for notes.html&index.html

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const util = require("util");
 const fs = require("fs");
+const path = require("path");
 const app = express();
 const PORT = 8080;
 
@@ -10,3 +11,16 @@ const writeFileAsync = util.promisify(fs.writeFile);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+//------------------------------------------
+//---------Routes to static HTML------------
+//------------------------------------------
+app.use(express.static(__dirname + "/public"))
+
+app.get("/notes", function(req, res){
+  res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
+
+app.get("/*", function(req, res){
+  res.sendFile(path.join(__dirname, "public", "index.html"))
+});
