aff15e7eed3b1d8a1aadbf30e3da588aa15a6c28
drg0drg <60710786+drg0drg@users.noreply.github.com>
2020-06-12 19:55:26 +0100

    setup express server

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const util = require("util");
 const fs = require("fs");
+const app = express();
+const PORT = 8080;
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
