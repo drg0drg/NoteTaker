5a9c2ac2e905fd4cbbad28ae0e81f062814cae40
drg0drg <60710786+drg0drg@users.noreply.github.com>
2020-06-12 21:12:13 +0100

    get method for notes in DB

--- server.js
+++ server.js
@@ -24,3 +24,10 @@ app.get("/notes", function(req, res){
 app.get("/", function(req, res){
   res.sendFile(path.join(__dirname, "public", "index.html"))
 });
+
+app.get("/api/notes", function(req, res){
+  const dataBaseLocation = path.join(__dirname, "db", "db.json");
+  const dataBaseInfo = readFileAsync(dataBaseLocation);
+  const parsedDataBase = JSON.parse(dataBaseInfo);
+  return res.json(parsedDataBase);
+})
