147cfb62bc6827cc8355ee40e4b2a65b594a40cb
drg0drg <60710786+drg0drg@users.noreply.github.com>
2020-06-12 22:13:49 +0100

    post method for new Note using Note class

--- server.js
+++ server.js
@@ -30,4 +30,13 @@ app.get("/api/notes", function(req, res){
   const dataBaseInfo = readFileAsync(dataBaseLocation);
   const parsedDataBase = JSON.parse(dataBaseInfo);
   return res.json(parsedDataBase);
+});
+
+app.post("/api/notes", function (req, res){
+  const dataBaseLocation = path.join(__dirname, "db", "db.json");
+  const dataBaseInfo = readFileAsync(dataBaseLocation);
+  const parsedDataBase = JSON.parse(dataBaseInfo);
+  const numberOfExistingNotes = parsedDataBase.length;
+  const newNoteID = numberOfExistingNotes +1; 
+  const newNote = new Note (newNoteID, req.body.noteTitle, req.body.noteContent);
 })
