1a46292db3e3574726cdc5268171d5970b3a1464
drg0drg <60710786+drg0drg@users.noreply.github.com>
2020-06-12 20:50:01 +0100

    addressing get method for index.html

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ app.get("/notes", function(req, res){
   res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 
-app.get("/*", function(req, res){
+app.get("/", function(req, res){
   res.sendFile(path.join(__dirname, "public", "index.html"))
 });
