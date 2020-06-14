47f2f3dd48a7f401bfc21234fce398e9f7307cb0
drg0drg <60710786+drg0drg@users.noreply.github.com>
2020-06-14 20:49:48 +0100

    Added comments to code

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const util = require("util");
 const fs = require("fs");
 const path = require("path");
 const app = express();
-const PORT = 8080;
+//setting up port for Heroku. <process.env.PORT> --> Heroku uses it's own port.
+const PORT = process.env.PORT || 8080;
 const Note = require("./noteClass");
 
 const readFileAsync = util.promisify(fs.readFile);
@@ -31,23 +32,29 @@ app.get("/", function (req, res) {
 
 app.get("/api/notes", async function (req, res) {
   try {
+    //reading the db.json file
     const dataBaseLocation = path.join(__dirname, "db", "db.json");
     const dataBaseInfo = await readFileAsync(dataBaseLocation);
+    //parsing the data in order to be returned
     const parsedDataBase = JSON.parse(dataBaseInfo);
-    return res.json(parsedDataBase);
+    //return the parsed data
+    return res.status(200).json(parsedDataBase);
   } catch (error) {
     console.log(error);
+    return res.status(404).send("Data not found");
   }
 });
 
+//API route with delete method
 app.post("/api/notes", async function (req, res) {
   try {
+    //reading the db.json file
     const dataBaseLocation = path.join(__dirname, "db", "db.json");
     const dataBaseInfo = await readFileAsync(dataBaseLocation);
     const parsedDataBase = JSON.parse(dataBaseInfo);
-    const numberOfExistingNotes = parsedDataBase.length;
+    //use the getNewNoteID function to create new ID for the new note
     const newNoteID = getNewNoteID(parsedDataBase);
-
+    //create new note using class "Note" passing in the title, text and the newNoteID
     const newNote = new Note(newNoteID, req.body.title, req.body.text);
     const parsedNewNote = newNote.getNote();
     parsedDataBase.push(parsedNewNote);
@@ -63,12 +70,16 @@ app.post("/api/notes", async function (req, res) {
   }
 });
 
+//API route with delete method
 app.delete("/api/notes/:id", async (req, res) => {
   try {
+    //Reading the db.json file
     const dataBaseLocation = path.join(__dirname, "db", "db.json");
     const dataBaseInfo = await readFileAsync(dataBaseLocation);
     const parsedDataBase = JSON.parse(dataBaseInfo);
+    //store the id in a variable
     const noteId = req.params.id;
+    //Filter method takes each note obj and stores into <newData> except the one with note.id == noteId (the one that is selected to be deleted)
     const newData = parsedDataBase.filter((note) => note.id !== noteId);
     const stringifiedDataBase = JSON.stringify(newData);
     await writeFileAsync(dataBaseLocation, stringifiedDataBase);
@@ -79,26 +90,35 @@ app.delete("/api/notes/:id", async (req, res) => {
   }
 });
 
+
+
+//------------------------------------------
+//---------Listener for server--------------
+//------------------------------------------
+
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
 
 
-
+//Function to create ID for a new note
 function getNewNoteID (parsedDataBase) {
+  //initialize the new note ID with zero
   let newNoteID = 0;
+  //if <parsedDataBase> exists AND has at least one object
   if (parsedDataBase && parsedDataBase.length>0) {
+    //pull all the id's from all the objects and store them into IDArray
     let IDArray = parsedDataBase.map(a => a.id);
-
+    //Method to determine the max ID in the array
     let max = IDArray.reduce(function(a, b) {
       return Math.max(a, b);
     });
-  
+    //Always increment the noteID. Each note will have a different ID
     newNoteID = max + 1;
     
   } else { 
+    //set the note ID
     newNoteID = 1;
   }
   return newNoteID;
-
 }
